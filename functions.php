bfb865bd653d745430d3a826f1dd1e6e6396dff1
Sebastiano Riva <sebastiano.riva@gmail.com>
2021-06-25 19:01:12 +0200

    added function rule, styling protected page login

--- functions.php
+++ functions.php
@@ -375,6 +375,28 @@ require get_template_directory() . '/inc/template-tags.php';
 require get_template_directory() . '/inc/customizer.php';
 
 /**
+ * Remove WordPress titles standard prefix.
+ */
+function the_title_trim($title) {
+    $title = attribute_escape($title);
+    $findthese = array(
+        '#Protected: #', // Notice the blank space after Protected:
+        '#Private: #', // Notice again, otherwise the title get pushed.
+		'#Protetto: #',
+		'#Privato: #'
+    );
+    $replacewith = array(
+        '', // What to replace "Protected: " with
+        '', // What to replace "Private: " with
+		'',
+		''
+    );
+    $title = preg_replace($findthese, $replacewith, $title);
+    return $title;
+}
+add_filter('the_title', 'the_title_trim');
+
+/**
  * [changed]
  * Define Blocks for Homepage, disabled
  */
